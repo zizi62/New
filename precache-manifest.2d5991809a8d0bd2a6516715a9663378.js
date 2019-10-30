4f6cd1828eb7f594b7c116a4ad7a2c9825a26391
Zoia Reutckaia <zizi2405@gmail.com>
2019-10-30 13:40:39 -0400

    Updates

---
+++ precache-manifest.2d5991809a8d0bd2a6516715a9663378.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07845bee94df2007f30f7dfcf3c9830",
+    "url": "/zizi62/New/index.html"
+  },
+  {
+    "revision": "393b5da8ff7d74a7712e",
+    "url": "/zizi62/New/static/css/3.3f19456c.chunk.css"
+  },
+  {
+    "revision": "272eb1dd48b593df89ac",
+    "url": "/zizi62/New/static/css/main.01262e22.chunk.css"
+  },
+  {
+    "revision": "c49aac7d191f1c2424cc",
+    "url": "/zizi62/New/static/js/2.33da1b1a.chunk.js"
+  },
+  {
+    "revision": "393b5da8ff7d74a7712e",
+    "url": "/zizi62/New/static/js/3.cee4e645.chunk.js"
+  },
+  {
+    "revision": "272eb1dd48b593df89ac",
+    "url": "/zizi62/New/static/js/main.fca4c6d6.chunk.js"
+  },
+  {
+    "revision": "499c05d28dbe416814ec",
+    "url": "/zizi62/New/static/js/runtime~main.0ac56528.js"
+  },
+  {
+    "revision": "a3469f27cbf658668e547ec91c5513e3",
+    "url": "/zizi62/New/static/media/preloader.a3469f27.svg"
+  }
+]);
